fbbb18f0b9f32bffbf406a79b57ccdf7d086b667
Jo├úo Pedro Maria <joaopedromaria182@gmail.com>
2023-02-13 20:22:01 -0300

    Criação de classe estatica e confiuguração do serviço build

---
+++ backend/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountOwnerServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options => 
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin()      //whithOrigines("Dominio")
+                    .AllowAnyMethod()      //WhitiMethods("POST", "GET")
+                    .AllowAnyHeader());    //WhitHeaders("accept", "content-type")     
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+            });
+        }
+    }
+}
--- backend/AccountOwnerServer/Program.cs
+++ backend/AccountOwnerServer/Program.cs
@@ -1,6 +1,11 @@
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,11 +17,22 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+
+app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders= ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
