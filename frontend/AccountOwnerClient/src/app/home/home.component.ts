96f7143d7dabe0075ecc0f2bfafe2c7599f232b7
joa0_182 <joaopedromaria182@gmail.com>
2023-03-30 19:39:03 -0300

    criando pasta frontend

---
+++ frontend/AccountOwnerClient/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public homeText: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.homeText ="BEM VINDO A APLICAÇÂO ACCOUNT-OWNER";
+  }
+}
